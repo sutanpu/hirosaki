a376794e4ae3f8b2412b78e357451d5a32c0dd37
ShoSampo <poohsan.pilot79@gmail.com>
2022-07-07 16:39:55 +0900

    first comit

---
+++ src/tmp/mysqli.php
@@ -0,0 +1,34 @@
+<?php
+
+$link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+
+if (!$link) {
+    echo 'Error: データベースに接続できませんでした' . PHP_EOL;
+    echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo 'データベースに接続できました' . PHP_EOL;
+
+$sql = 'SELECT name, founder FROM companies';
+$result = mysqli_query($link, $sql);
+
+while ($company = mysqli_fetch_assoc($result)) {
+    echo '会社名：' . $company['name'] . PHP_EOL;
+    echo '代表者名：' . $company['founder'] . PHP_EOL;
+}
+
+mysqli_free_result($result);
+
+
+
+// $result = mysqli_query($link, $sql);
+// if ($result) {
+//     echo 'データを追加しました' . PHP_EOL;
+// } else {
+//     echo 'Error: データの追加に失敗しました' . PHP_EOL;
+//     echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+// }
+
+mysqli_close($link);
+echo 'データベースとの接続を切断しました' . PHP_EOL;
